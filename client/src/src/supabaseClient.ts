b163d74aa4b0e5550099ca3fa92f4b69ba2dd286
DF791021 <122670790+DF791021@users.noreply.github.com>
2026-04-19 06:50:07 -0500

    Deleted supabaseClient.ts

--- client/src/src/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://womkqcqdblrulqkrryfu.supabase.co'
-const supabaseAnonKey = 'PASTE_YOUR_ANON_KEY_HERE'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
